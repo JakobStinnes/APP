876a7ca7932f2ff7332804906744f96d3e127d17
JakobStinnes <128197613+JakobStinnes@users.noreply.github.com>
2023-10-19 16:22:43 +0200

    Conncetion to Rest API

---
+++ PV/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  PV
+//
+//  Created by Jakob Stinnes on 19.10.23.
+//
+
+import Foundation
---
+++ PV/Locations.swift
@@ -0,0 +1,8 @@
+//
+//  Locations.swift
+//  PV
+//
+//  Created by Jakob Stinnes on 19.10.23.
+//
+
+import Foundation
