c21a03257fef4406fe1bc4f863bafe90b6fd9291
JakobStinnes <128197613+JakobStinnes@users.noreply.github.com>
2023-10-19 14:28:39 +0200

    Initial Commit

---
+++ PV/PVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PVApp.swift
+//  PV
+//
+//  Created by Jakob Stinnes on 19.10.23.
+//
+
+import SwiftUI
+
+@main
+struct PVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
